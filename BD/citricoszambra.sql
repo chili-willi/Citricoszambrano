fdb8160e9e4312b3aceaae5eab8bc2ef5eb231c8
chili-willi <209413257+chili-willi@users.noreply.github.com>
2025-06-06 21:13:18 -0500

    Se adiciona bd

---
+++ BD/citricoszambra.sql
@@ -0,0 +1,42 @@
+CREATE database citricoszambra;
+
+USE citricoszambra;
+
+
+CREATE TABLE `cliente` (
+  `idcliente` int(11) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `telefono` int(15) NOT NULL,
+  `fecha_registro` date DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+CREATE TABLE `formulario` (
+  `id_formulario` int(11) NOT NULL,
+  `idcliente` int(11) NOT NULL,
+  `tipo_consulta` varchar(50) DEFAULT NULL,
+  `mensaje` text NOT NULL,
+  `fecha_envio` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idcliente`);
+
+ALTER TABLE `formulario`
+  ADD PRIMARY KEY (`id_formulario`),
+  ADD KEY `idcliente` (`idcliente`);
+
+
+ALTER TABLE `cliente`
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `formulario`
+  MODIFY `id_formulario` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `formulario`
+  ADD CONSTRAINT `formulario_ibfk_1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`);
+COMMIT;
